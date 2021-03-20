c06b20d38e443505ab4e8b1c6f258e1ec1f85446
con0fav <connor.favero@gmail.com>
2021-03-19 22:57:48 -0700

    commit to github, adding components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Jumbotron from './components/jumbotron';
+
+function App() {
+  return (
+    <div className="container">
+      <Jumbotron/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/jumbotron.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Jumbotron() {
+  return (
+    <div className="jumbotron">
+      <h1>Connor Favero Portfolio</h1>
+      <p>
+        Holy crackers I'm making a React app.
+      </p>
+    </div>
+  );
+}
+
+export default Jumbotron;

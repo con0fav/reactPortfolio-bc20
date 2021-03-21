75acb23a5fb8ca3a47a2127889cfeef1ec71ad32
con0fav <connor.favero@gmail.com>
2021-03-20 22:24:26 -0700

    adding card component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Jumbotron from './components/jumbotron';
+import Card from './components/card'
 
 function App() {
   return (
     <div className="container">
       <Jumbotron/>
+      <Card/>
     </div>
   );
 }
---
+++ src/components/card.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Card() {
+
+    return (
+
+        <div class="card mb-3">
+            <img src="..." class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+        </div>
+
+    )
+}
+
+export default Card;

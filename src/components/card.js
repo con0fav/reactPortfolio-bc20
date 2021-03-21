8a142707b9af5bcaf0a7e11d32b18cb0e9d3c4a2
con0fav <connor.favero@gmail.com>
2021-03-20 22:41:06 -0700

    multiple cards

--- src/components/card.js
+++ src/components/card.js
@@ -4,12 +4,33 @@ function Card() {
 
     return (
 
-        <div class="card mb-3">
-            <img src="..." class="card-img-top" alt="..."/>
+        <div class="container">
+
+
+            <div class="card mb-3">
+                <img src="..." class="card-img-top" alt="..." />
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                 </div>
+            </div>
+
+            <div class="card mb-3">
+                <img src="..." class="card-img-top" alt="..." />
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+
+            <div class="card mb-3">
+                <img src="..." class="card-img-top" alt="..." />
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+
         </div>
 
     )
